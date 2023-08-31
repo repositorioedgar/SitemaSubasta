a4ce6d48f37c994d386e05f537fdce9768acb077
repositorioedgar <142647158+repositorioedgar@users.noreply.github.com>
2023-08-31 12:26:48 -0400

    Add files via upload

---
+++ SitemaSubasta/Oferta.java
@@ -0,0 +1,22 @@
+
+/**
+ * Univ. Edgar Condori Polloquiri
+ */
+public class Oferta
+{
+   private Persona ofertante;
+   private long monto;
+   public Oferta(Persona ofertante, long monto)
+   {
+       this.ofertante = ofertante;
+       this.monto = monto;
+   }
+   public long getMonto()
+   {
+       return monto;
+   }
+   public String getNombre()
+   {
+       return ofertante.getPersona();
+   }
+}
